5989ab92c0321b196a7de7686b47d005fdbdb192
Alex Jung <alexandermjung89@gmail.com>
2017-07-31 14:34:35 -0500

    got yellow background in, not enough time to get out

--- script.js
+++ script.js
@@ -17,6 +17,11 @@ removeDivAndButtons();
 console.log('delete clicked');
 })
 
+//swap button should toggle colors
+$('#box-fields').on('click','#swap', function(){
+toggleColors();
+console.log('swap clicked');
+})
 
 var addDivsAndButtons = function(){
 $('#box-fields').append('<div class="box"><p>' + click + '</p><p><button id="swap">Swap</button><button id="delete">Delete</button></p></div>');
@@ -27,5 +32,10 @@ var removeDivAndButtons =function (){
 $('#delete').parent().parent().remove();
 console.log('removal run');
 };//end of removeDivAndButtons
+
+var toggleColors = function (){
+$('.box').css('background-color','yellow')
+    //need to rename the class of the boxes and style them within the css. not enough time.
+};//end of toggleColors
 
 }); // end of document
